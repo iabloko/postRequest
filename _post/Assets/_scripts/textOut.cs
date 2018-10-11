e39f627083061d9e6c90118752a02a4018972331
Dxsunty <Dxsunty@gmail.com>
2018-10-11 09:00:47 +0400

    #pull1

---
+++ _post/Assets/_scripts/textOut.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class textOut : MonoBehaviour {
+
+	public Text _Text;
+
+	void Start () {
+		//StartCoroutine(Upload());
+	}
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Q))
+			StartCoroutine (POST ());
+		if (Input.GetKeyUp (KeyCode.E))
+			StartCoroutine (GET ());
+	}
+
+	public IEnumerator POST () {
+		Debug.Log("Post_");
+		var Data = new WWWForm ();
+		Data.AddField ("«action»:", "check");
+		Data.AddField ("«deviceName»:", "north_entrance");
+		Data.AddField ("barcode", "1234560000123");
+
+		var Query = new WWW ("vk.com", Data);
+		yield return Query;
+		if (Query.error != null) {
+			Debug.Log ("Server does not respond : " + Query.error);
+		} else {
+			if (Query.text == "Testss") {//ответ сервера
+				Debug.Log ("Server responded correctly");
+			} else {
+				Debug.Log ("Server responded : " + Query.text);
+			}
+		}
+		Query.Dispose ();
+	}
+
+	public IEnumerator GET () {
+		Debug.Log("_GET");
+		string data1 = "Текст 1";
+		string data2 = "Текст 2";
+		WWW Query = new WWW ("vk.com"); //+ data1 + "&variable2=" + data2);
+		yield return Query;
+		if (Query.error != null) {
+			Debug.Log ("Server does not respond : " + Query.error);
+		} else {
+			if (Query.text == "Test") {
+				Debug.Log ("Server responded correctly");
+			} else {
+				Debug.Log ("Server responded : " + Query.text);
+			}
+		}
+		Query.Dispose ();
+	}
+}
