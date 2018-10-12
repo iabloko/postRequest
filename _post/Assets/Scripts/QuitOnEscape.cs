caa2639b7ad5fa3f7ebc8b4772d7bfa0dbb73e0d
iabloko <31312961+iabloko@users.noreply.github.com>
2018-10-12 11:16:30 +0300

    save

---
+++ _post/Assets/Scripts/QuitOnEscape.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class QuitOnEscape : MonoBehaviour {
+
+    private bool quit = false;
+    public textOut _textOut;
+    void Start () {
+        quit = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyUp (KeyCode.Escape)) {
+            Debug.Log("EXIT");
+            Application.Quit ();
+        }
+    }
+}
--- _post/Assets/Scripts/textOut.cs
+++ _post/Assets/Scripts/textOut.cs
@@ -7,10 +7,16 @@ using UnityEngine.UI;
 public class textOut : MonoBehaviour {
 	private string JsonDataString;
 	public Text _Text;
+	public GameObject _IPOBJ;
 	public string _url;
+	[HideInInspector] public bool MenuActive = false;
+
+	void Awake () {
+		_url = PlayerPrefs.GetString ("_url", _url);
+	}
 
 	void Start () {
-
+		_IPOBJ.SetActive (false);
 	}
 	void Update () {
 		if (Input.GetKeyUp (KeyCode.Q))
@@ -19,18 +25,19 @@ public class textOut : MonoBehaviour {
 			StartCoroutine (GET ());
 		if (Input.GetKeyUp (KeyCode.W))
 			StartCoroutine (_UnityWebRequest ());
-
+		if (Input.GetKeyUp (KeyCode.Tab))
+			_MenuActive (_IPOBJ);
 	}
 
 	public IEnumerator POST () {
-		Debug.Log ("Post");
+		_Text.text = "Post";
 		var Data = new WWWForm ();
 		Data.AddField ("action", "check");
 		Data.AddField ("deviceName", "north_entrance");
 		Data.AddField ("param1", "param1value");
-
 		var Query = new WWW (_url, Data);
 		yield return Query;
+
 		if (Query.error != null) {
 			Debug.Log ("Server does not respond : " + Query.error);
 		} else {
@@ -46,11 +53,12 @@ public class textOut : MonoBehaviour {
 	}
 
 	public IEnumerator GET () {
-		Debug.Log ("Get");
+		_Text.text = "GET";
 		//string data1 = "Текст 1";
 		//string data2 = "Текст 2";
 		WWW Query = new WWW (_url); //+ data1 + "&variable2=" + data2);
 		yield return Query;
+
 		if (Query.error != null) {
 			Debug.Log ("Server does not respond : " + Query.error);
 		} else {
@@ -66,18 +74,35 @@ public class textOut : MonoBehaviour {
 	}
 
 	IEnumerator _UnityWebRequest () {
-		Debug.Log ("_UnityWebRequest");
+		_Text.text = "_UnityWebRequest";
 		using (UnityWebRequest www = UnityWebRequest.Get (_url)) {
 			yield return www.SendWebRequest ();
 
 			if (www.isNetworkError || www.isHttpError) {
 				Debug.Log (www.error);
 			} else {
-				Debug.Log (www.downloadHandler.text);
-				_Text.text = www.downloadHandler.text;
+				//_Text.text = www.downloadHandler.text;
 				byte[] results = www.downloadHandler.data;
+				JSONNode jsonNode = SimpleJSON.JSON.Parse (www.downloadHandler.text);
+				_Text.text = jsonNode["country"].ToString ().ToUpper ();
 			}
 			www.Dispose ();
 		}
+	}
+
+	private void _MenuActive (GameObject _InputField) {
+
+		if (_InputField.activeInHierarchy == false)
+			_IPOBJ.SetActive (true);
+		else
+			_IPOBJ.SetActive (false);
+	}
+
+	public void SaveChange () {
+		InputField _IFfield = _IPOBJ.GetComponent<InputField> () as InputField;
+		_url = _IFfield.text;
+
+		PlayerPrefs.SetString ("_url", _IFfield.text);
+		Debug.Log ("IPField.text" + "              " + _url);
 	}
 }
