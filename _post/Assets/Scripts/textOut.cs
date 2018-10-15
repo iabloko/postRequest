53f19130b9d4b76c4a0511847f89a18aebe85902
iabloko <31312961+iabloko@users.noreply.github.com>
2018-10-15 12:47:09 +0300

    addUriCheck

--- _post/Assets/Scripts/textOut.cs
+++ _post/Assets/Scripts/textOut.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
@@ -6,11 +7,13 @@ using UnityEngine.UI;
 
 namespace Kulikovskoe {
 	public class textOut : MonoBehaviour {
+		//public MenuAnimator _MenuAnimator;
 		private string JsonDataString;
 		public Toggle _AutoPlay;
 		public Text _Text, _CurrentURL;
 		public GameObject _IPOBJ;
 		public string _url;
+		private Uri _uri;
 		private int _i = 1;
 		[HideInInspector] public bool _Request = true;
 
@@ -19,8 +22,7 @@ namespace Kulikovskoe {
 		void Awake () {
 			LoadChanges ();
 			_IPOBJ.SetActive (false);
-
-			_CurrentURL.text = " CurrentURL: " + " " + _url;
+			_CurrentURL.text = " CurrentURL: " + _url;
 		}
 
 		void Update () {
@@ -29,13 +31,18 @@ namespace Kulikovskoe {
 			}
 
 			if (_elapsed >= 1.2f && _AutoPlay.isOn == false && _Request == true) {
-				_Request = false;
-				_Text.text = "Connect";
-				_elapsed = 0;
-				StartCoroutine (POST ());
+				if (!Uri.TryCreate (_url, UriKind.Absolute, out _uri)) {
+					_Text.text = "Invalid URL. Example: 192.200.100.252:8088/api/external, or vk.com";
+				} else {
+					_Request = false;
+					_Text.text = "Connect";
+					_elapsed = 0;
+					StartCoroutine (POST ());
+				}
 			} else if (_elapsed >= 5)
 				_elapsed = 0;
 			_elapsed += Time.deltaTime;
+
 		}
 
 		public IEnumerator POST () {
@@ -44,23 +51,23 @@ namespace Kulikovskoe {
 			Data.AddField ("deviceName", "north_entrance");
 			var Query = new WWW (_url, Data);
 			yield return Query;
-			_Request = true;
 
 			if (Query.error != null) {
 				_Text.text = "Server does not respond : " + Query.error;
+				_Request = true;
 			} else {
 				_Text.text = "Server responded : " + Query.text;
 				JSONNode jsonNode = Kulikovskoe.JSON.Parse (Query.text);
 				_Text.text = jsonNode["success"].ToString ().ToUpper ();
 				if (_Text.text == "true") {
-					//запуск видео
+					//_MenuAnimator._StartAnimation ();
 				}
 			}
 			Query.Dispose ();
 		}
 
-		private void _MenuActive (GameObject _InputField) {
-			if (_InputField.activeInHierarchy == false)
+		private void _MenuActive (GameObject _panel) {
+			if (_panel.activeInHierarchy == false)
 				_IPOBJ.SetActive (true);
 			else
 				_IPOBJ.SetActive (false);
@@ -78,6 +85,5 @@ namespace Kulikovskoe {
 			_AutoPlay.isOn = PlayerPrefs.GetInt ("_AutoPlay.isOn") == 1 ? true : false;
 			_url = PlayerPrefs.GetString ("_url", "http://" + _url);
 		}
-
 	}
 }
