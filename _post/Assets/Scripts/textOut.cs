79c54e87c9f7feaad395981cb54e467c25fe7906
iabloko <31312961+iabloko@users.noreply.github.com>
2018-10-15 11:29:04 +0300

    simpleFix

--- _post/Assets/Scripts/textOut.cs
+++ _post/Assets/Scripts/textOut.cs
@@ -6,80 +6,60 @@ using UnityEngine.UI;
 
 namespace Kulikovskoe {
 	public class textOut : MonoBehaviour {
-		//public MenuAnimator _MenuAnimator;
 		private string JsonDataString;
 		public Toggle _AutoPlay;
 		public Text _Text, _CurrentURL;
 		public GameObject _IPOBJ;
 		public string _url;
-
 		private int _i = 1;
-		public bool _Request = true;
-		[HideInInspector] public bool MenuActive = false;
+		[HideInInspector] public bool _Request = true;
 
-		//
+		private float _elapsed;
 
 		void Awake () {
 			LoadChanges ();
 			_IPOBJ.SetActive (false);
-		}
 
-		void Start () {
-
+			_CurrentURL.text = " CurrentURL: " + " " + _url;
 		}
 
 		void Update () {
-			if (Input.GetKeyUp (KeyCode.Q)) {
-				StartCoroutine (POST ());
-			}
-
 			if (Input.GetKeyUp (KeyCode.Escape) || Input.GetKeyUp (KeyCode.Tab)) {
 				_MenuActive (_IPOBJ);
 			}
 
-			if ((_i % 301 == 0) && _AutoPlay.isOn == false && _Request == true) {
-				_i = 1;
+			if (_elapsed >= 1.2f && _AutoPlay.isOn == false && _Request == true) {
 				_Request = false;
+				_Text.text = "Connect";
+				_elapsed = 0;
 				StartCoroutine (POST ());
-			} else if (_i == 50000) {
-				_i = 1;
-			}
-			_i++;
+			} else if (_elapsed >= 5)
+				_elapsed = 0;
+			_elapsed += Time.deltaTime;
 		}
 
 		public IEnumerator POST () {
-			_i = 1;
 			var Data = new WWWForm ();
 			Data.AddField ("action", "check");
 			Data.AddField ("deviceName", "north_entrance");
 			var Query = new WWW (_url, Data);
 			yield return Query;
+			_Request = true;
 
 			if (Query.error != null) {
-				Debug.Log ("Server does not respond : " + Query.error);
-				_Text.text = Query.error;
-				_Request = true;
+				_Text.text = "Server does not respond : " + Query.error;
 			} else {
-				if (Query.text == "Testss") { //ответ сервера
-					Debug.Log ("Server responded correctly");
-					_Request = true;
-				} else {
-					Debug.Log ("Server responded : " + Query.text);
-					_Text.text = Query.text;
-					JSONNode jsonNode = Kulikovskoe.JSON.Parse (Query.text);
-					_Text.text = jsonNode["success"].ToString ().ToUpper ();
-					if (_Text.text == "true") {
-						//_MenuAnimator.SA ();
-					} else {
-						_Request = true;
-					}
+				_Text.text = "Server responded : " + Query.text;
+				JSONNode jsonNode = Kulikovskoe.JSON.Parse (Query.text);
+				_Text.text = jsonNode["success"].ToString ().ToUpper ();
+				if (_Text.text == "true") {
+					//запуск видео
 				}
 			}
 			Query.Dispose ();
 		}
 
 		private void _MenuActive (GameObject _InputField) {
-
 			if (_InputField.activeInHierarchy == false)
 				_IPOBJ.SetActive (true);
 			else
@@ -88,19 +68,15 @@ namespace Kulikovskoe {
 
 		public void SaveChange () {
 			InputField _IFfield = _IPOBJ.GetComponentInChildren<InputField> () as InputField;
-			//_url =_IFfield.text;
-
-			PlayerPrefs.SetString ("_CurrentURL", _url);
 			PlayerPrefs.SetInt ("_AutoPlay.isOn", _AutoPlay.isOn?1 : 0);
-			PlayerPrefs.SetString ("_url", "http://"+_IFfield.text);
-			SceneManager.LoadScene ("Kulikovskoe360-1");
+			PlayerPrefs.SetString ("_url", "http://" + _IFfield.text);
+
+			SceneManager.LoadScene ("WebRequest");
 		}
 		private void LoadChanges () {
 			_Request = true;
-
-			_CurrentURL.text = "CurrentURL:" + PlayerPrefs.GetString ("_CurrentURL", _CurrentURL.text);
 			_AutoPlay.isOn = PlayerPrefs.GetInt ("_AutoPlay.isOn") == 1 ? true : false;
-			_url = PlayerPrefs.GetString ("_url", "http://"+_url);
+			_url = PlayerPrefs.GetString ("_url", "http://" + _url);
 		}
 
 	}
